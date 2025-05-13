2dd2fd9626479577384db69c9729e0324b58c707
Sam Earle <smearle93@gmail.com>
2025-05-13 14:27:29 -0400

    Work on profiling standalone nodejs (having an issue with second call to compile())

--- makefile
+++ makefile
@@ -1,4 +1,3 @@
 sync_data_210:
 	rsync -arv ./data/js_sols 210:~/script-doctor/data && \
-	rsync -arv ./data/parse_results*.json 210:~/script-doctor/data/ && \
 	rsync -arv 210:~/script-doctor/data ./
--- profile_standalone_nodejs.py
+++ profile_standalone_nodejs.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import random
@@ -11,6 +12,10 @@ from utils import get_list_of_games_for_testing
 ps = require('./standalone/puzzlescript/engine.js')
 solver = require('./standalone/puzzlescript/solver.js')
 
+STANDALONE_NODEJS_RESULTS_PATH = os.path.join('data', 'standalone_nodejs_results.json')
+
+# algos = [solver.solveBFS, solver.solveAStar, solver.solveMCTS]:
+algos = [solver.solveBFS]
 n_steps = 20_000
 
 def compile_game(game, level_i):
@@ -19,14 +24,40 @@ def compile_game(game, level_i):
         game_text = f.read()
     ps.compile(game_text, level_i)
 
+def get_algo_name(algo):
+    return str(algo).split(' ')[1].strip(']')
+
 def main():
     games_to_test = get_list_of_games_for_testing(all_games=True)
+    results = {get_algo_name(algo): {game: {} for game in games_to_test} for algo in algos }
+    print(results)
     for game in games_to_test:
-        compile_game(game, 0)
 
-        for algo in [solver.solveBFS, solver.solveAStar, solver.solveMCTS]:
-            result = algo(ps)
+        print(f'Game: {game}')
+        # TODO: How to get the available number of levels from nodejs?
+        level_i = 0
+        print(f'Level: {level_i}')
+        for algo in [solver.solveBFS]:
+            algo_name = get_algo_name(algo)
+            print(f'Algorithm: {algo_name}')
+            compile_game(game, 0)
+            result = algo(ps, timeout=1000)
+            result = {
+                # 'game': game,
+                # 'level': level_i,
+                # 'algo': algo.__name__,
+                'solved': result[0],
+                'actions': result[1],
+                'iterations': result[2],
+                'time': result[3],
+                'FPS': result[2] / result[3],
+            }
             print(result)
+            if level_i not in results[algo_name][game]:
+                results[algo_name][level_i] = result
+
+            with open(STANDALONE_NODEJS_RESULTS_PATH, 'w') as f:
+                json.dump(results, f, indent=4)
 
 
 
